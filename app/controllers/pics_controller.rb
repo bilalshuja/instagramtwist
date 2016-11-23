ecdee6b7dfcd71fe8e835c708c91e8e2704943e1
bilalshuja <bilalshuja10@gmail.com>
2016-11-23 11:38:24 -0800

    Edit, Update and Destroy Actions and VIew Files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,41 +1,51 @@
 class PicsController < ApplicationController
-before_action :find_pic, only:[:show, :edit, :update, :destroy]
+ before_action :find_pic, only:[:show, :edit, :update, :destroy]
 
-def index
+ def index
+ @pics = Pic.all.order("created_at DESC")
+ end
+ def show
+ end
 
-end
-def show
-
-
-end
-
-def new
-@pic = Pic.new
-
-	end
+ def new
+ @pic = Pic.new
+ end
 
 
 
 
 
-def create
+ def create
 	@pic= Pic.new(pic_params)
 	if @pic.save
 		redirect_to @pic, notice:"yes'it was posted"
 	else
 		render "new"
 	end
-end
+ end
+ def edit
+ end
+
+ def update
+	if @pic.update(pic_params)
+		redirect_to @pic, notice:"congrats"
+    else
+    	render 'edit'
+    end
+ end	
+ def destroy
+  @pic.destroy
+  redirect_to root_path
+ 
+ end
 
  private
 
-   def pic_params
-
-	params.require(:pic).permit(:title, :description)
-   end
-   def find_pic
-   	@pic = Pic.find(params[:id])
-   end
-
+  def pic_params
+   params.require(:pic).permit(:title, :description)
+  end
+  def find_pic
+   @pic = Pic.find(params[:id])
+  end
 
 end
