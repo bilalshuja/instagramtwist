02dd183538ac7ebddd2cef9bc0c4a132f7590f49
bilalshuja <bilalshuja10@gmail.com>
2016-11-24 03:08:37 -0800

    Add devise and associations with Pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -8,7 +8,7 @@ class PicsController < ApplicationController
  end
 
  def new
- @pic = Pic.new
+ @pic = current_user.pics.build
  end
 
 
@@ -16,7 +16,7 @@ class PicsController < ApplicationController
 
 
  def create
-	@pic= Pic.new(pic_params)
+	@pic= current_user.pics.build(pic_params)
 	if @pic.save
 		redirect_to @pic, notice:"yes'it was posted"
 	else
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+ has_many :pics 
+ 
+
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
